a6ed45f400ec5d0b28ae38d2524f5ec0053b6fcf
Maciej Kurek <maciejk77@gmail.com>
2023-07-28 08:14:25 +0100

    Fix style

--- pages/components/Loader/index.tsx
+++ pages/components/Loader/index.tsx
@@ -1,15 +1,10 @@
 import { FunctionComponent } from 'react';
-// import { StyledLoading } from './styles';
+import { StyledLoading } from './styles';
 
 const Loader: FunctionComponent = () => {
   return (
     <>
-      {/* inline below, styled not working */}
-      {/* <StyledLoading> */}
-      <div style={{ fontSize: 15, padding: 5, fontWeight: 'bold' }}>
-        Loading...
-      </div>
-      {/* </StyledLoading> */}
+      <StyledLoading>Loading...</StyledLoading>
     </>
   );
 };
