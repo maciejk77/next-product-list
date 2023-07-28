355f57fdd8bd7a4244e5840298171160e9f0821e
Maciej Kurek <maciejk77@gmail.com>
2023-07-28 10:04:12 +0100

    Add Lato fonts

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,13 +2,21 @@ import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import { useApollo } from '../apollo';
 import { ApolloProvider } from '@apollo/client';
+import { Lato } from 'next/font/google';
+
+const fonts = Lato({
+  weight: '400',
+  subsets: ['latin'],
+});
 
 function MyApp({ Component, pageProps }: AppProps) {
   const client = useApollo();
 
   return (
     <ApolloProvider client={client}>
-      <Component {...pageProps} />
+      <main className={fonts.className}>
+        <Component {...pageProps} />
+      </main>
     </ApolloProvider>
   );
 }
