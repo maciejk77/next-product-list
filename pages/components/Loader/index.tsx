400fe8a0603f93b9802494bcd5da78312ff0b49e
Maciej Kurek <maciejk77@gmail.com>
2023-07-28 08:24:15 +0100

    Revert inline style

--- pages/components/Loader/index.tsx
+++ pages/components/Loader/index.tsx
@@ -1,10 +1,22 @@
 import { FunctionComponent } from 'react';
-import { StyledLoading } from './styles';
+// import { StyledLoading } from './styles';
 
 const Loader: FunctionComponent = () => {
   return (
     <>
-      <StyledLoading>Loading...</StyledLoading>
+      {/* inline below, styled not working */}
+      {/* <StyledLoading> */}
+      <div
+        style={{
+          fontSize: 15,
+          padding: 5,
+          fontWeight: 'bold',
+          backgroundColor: 'white',
+        }}
+      >
+        Loading...
+      </div>
+      {/* </StyledLoading> */}
     </>
   );
 };
